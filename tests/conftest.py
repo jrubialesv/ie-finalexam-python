19e62150d90c357e4220a9a3825dcd06fd9b95ce
jrubialesv <jrubialesv@gmail.com>
2022-12-15 17:00:47 +0100

    m

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,21 +2,6 @@ import pytest
 from iebank_api.models import Account
 from iebank_api import db, app
 
-
-@pytest.fixture
-def testing_client(scope='module'):
-    db.create_all()
-    account = Account('Test Account', '€')
-    db.session.add(account)
-    db.session.commit()
-
-    with app.test_client() as testing_client:
-        yield testing_client
-
-    db.drop_all()
-
-    # Modify the existing fixture so that it is executed for every test case 
-
 @pytest.fixture
 def testing_client(scope='function'):
     db.create_all()
